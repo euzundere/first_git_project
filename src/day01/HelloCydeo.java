f2e232affe5339bc29a3a1b1ab7edfd2cca0ca0d
euzundere <emre.uzundere3@gmail.com>
2025-06-14 00:48:20 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -5,5 +5,7 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeos");
+        System.out.println("Hello Java");
+        System.out.println("Hello Programmer");
     }
 }
